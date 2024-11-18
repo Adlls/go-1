bb8e2da698ce438c7bdd130b8a909a2ef4e644a5
Adlan <asaidullaev@skolopendra.com>
2024-11-18 19:53:19 +0300

    check server stats

--- main.go
+++ main.go
@@ -1,3 +1,123 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	host = "http://srv.msk01.gigacorp.local/_stats"
+)
+
+var errorCount = 0
+
+func main() {
+	for {
+		responseInSplit := requestToServer(host)
+
+		proccessingStatsData(parseAndGetArrayFromResponse(responseInSplit))
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func requestToServer(host string) []string {
+	response, error := http.Get(host)
+
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		errorCount++
+	}
+	response.Body.Close()
+
+	return strings.Split(strings.Trim(string(body), "  \n"), ",")
+}
+
+func parseAndGetArrayFromResponse(responseInSplit []string) [7]int64 {
+	loadAverage, err := strconv.ParseInt(responseInSplit[0], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	memoryValue, err := strconv.ParseInt(responseInSplit[1], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	memoryUsage, err := strconv.ParseInt(responseInSplit[2], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+	diskValue, err := strconv.ParseInt(responseInSplit[3], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	diskUsage, err := strconv.ParseInt(responseInSplit[4], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	networkValue, err := strconv.ParseInt(responseInSplit[5], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	networkUsage, err := strconv.ParseInt(responseInSplit[6], 10, 64)
+	if err != nil {
+		errorCount++
+	}
+
+	return [7]int64{loadAverage, memoryValue, memoryUsage, diskValue, diskUsage, networkValue, networkUsage}
+}
+
+func proccessingStatsData(responseInInt64 [7]int64) {
+	if errorCount >= 3 {
+		fmt.Println("Unable to fetch server statistic")
+
+		return
+	}
+
+	if responseInInt64[0] > 30 {
+		fmt.Println("Load Average is too high:", responseInInt64[0])
+	}
+
+	capacityUsageMemory := float64(responseInInt64[2]) / float64(responseInInt64[1])
+
+	if capacityUsageMemory > 0.8 {
+		fmt.Printf("Memory usage too high: %d%\n", (int)(capacityUsageMemory*100))
+	}
+	capacityUsageDisk := float64(responseInInt64[3]) / float64(responseInInt64[2])
+	freeDiskSpace := responseInInt64[3] - responseInInt64[2]
+	if 0 > freeDiskSpace {
+		freeDiskSpace = 0
+	}
+
+	if capacityUsageDisk > 0.9 {
+		fmt.Printf("Free disk space is too low %d Mb left\n", (int)(freeDiskSpace/1_000_000))
+	}
+
+	networkCapacity := float64(responseInInt64[4]) / float64(responseInInt64[3])
+
+	freeNetworkValue := responseInInt64[4] - responseInInt64[3]
+
+	if 0 > freeNetworkValue {
+		freeNetworkValue = 0
+	}
+
+	if networkCapacity > 0.9 {
+		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", (int)(freeDiskSpace/125_000))
+	}
+	errorCount = 0
+}
